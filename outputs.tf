6be229b62db8213a9f5d1bb11f299921206f16f8
peric <drazen.peric@kiwi.com>
2020-04-28 12:36:19 +0200

    Add port to output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "kibana_endpoint" {
-  value = "${var.cluster_name}-kibana.${var.namespace}.svc.cluster.local"
+  value = "${var.cluster_name}-kibana.${var.namespace}.svc.cluster.local:5601"
 }
